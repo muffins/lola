32998ebd72cec0aafa82c95c934f9d24dcd2adbe
Nick Anderson <nba237@nyu.edu>
2015-07-01 19:11:46 -0600

    Adding initial Solution files

---
+++ lola/lola/lola.cpp
@@ -0,0 +1,39 @@
+/*
+ * Lola Malware
+ *
+ * Nick Anderson - 2015
+ *
+ * This malware was written for the 2015 CSAW High School forensics challenge.  The malware
+ * will drop to disk, and pull down a file from a C2 server.  The file pulled will contain
+ * further instructions for the malware to carry out.
+ *
+ * TODO: We need to implement the following functionalities
+ *	- Beacon to C2
+ *  - Persistence
+ *  - Stealth/Hiding Mech
+ *  
+ *
+ */
+
+
+
+#include <iostream>
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[]){
+
+
+
+
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+## CSAW High School Forensics Challenge - Lola Malware
+
+### This code was written for the 2015 CSAW High School Forensics Prelims
+
+
+### About
